d233422193337e151465ba6598bea3dc152e440b
techmoves <muhndlovu28@gmail.com>
2023-10-09 16:26:29 +0200

    Updated the styling

--- src/login.js
+++ src/login.js
@@ -1,53 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const LoginPage = () => (
-  <div className="flex justify-center items-center h-screen">
-    <div className="w-1/3">
-      <div className="bg-white shadow-md rounded px-8 py-6">
-        <h3 className="text-2xl font-bold mb-6 text-center">Login</h3>
-        <form>
-          <div className="mb-4">
-            <label
-              htmlFor="username"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-
-              Username
-            </label>
-            <input
-              type="text"
-              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-            />
+  <div className="container">
+    <div className="row justify-content-center mt-5">
+      <div className="col-md-6">
+        <div className="card">
+          <div className="card-header">
+            <h3 className="text-center">Login</h3>
           </div>
-          <div className="mb-6">
-            <label
-              htmlFor="password"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-            />
+          <div className="card-body">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="username" className="form-label">
+                  Username
+                </label>
+                <input type="text" className="form-control" id="username" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input type="password" className="form-control" id="password" />
+              </div>
+              <div className="mb-3">
+                <button type="submit" className="btn btn-primary">
+                  Sign In
+                </button>
+                <a className="btn btn-link" href="#">
+                  Forgot Password?
+                </a>
+              </div>
+            </form>
           </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
-              Sign In
-            </button>
-            <a
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="#"
-            >
-              Forgot Password?
-            </a>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   </div>
