669e382c5a6023d83d6da4de5e68523dd547ac93
techmoves <muhndlovu28@gmail.com>
2023-10-09 20:14:10 +0200

    Updated login page to have user name only as per requirement

--- src/login.js
+++ src/login.js
@@ -1,41 +1,45 @@
-import React from "react";
+import React from 'react';
 
-const LoginPage = () => (
-  <div className="container">
-    <div className="row justify-content-center mt-5">
-      <div className="col-md-6">
-        <div className="card">
-          <div className="card-header">
-            <h3 className="text-center">Login</h3>
-          </div>
-          <div className="card-body">
-            <form>
-              <div className="mb-3">
-                <label htmlFor="username" className="form-label">
-                  Username
-                </label>
-                <input type="text" className="form-control" id="username" />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="password" className="form-label">
-                  Password
-                </label>
-                <input type="password" className="form-control" id="password" />
-              </div>
-              <div className="mb-3">
-                <button type="submit" className="btn btn-primary">
-                  Sign In
-                </button>
-                <a className="btn btn-link" href="#">
-                  Forgot Password?
-                </a>
-              </div>
-            </form>
+const LoginPage = () => {
+  const handleForgotPassword = () => {
+    // Add your desired code here for handling the "Forgot Password?" action
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header">
+              <h3 className="text-center">Login</h3>
+            </div>
+            <div className="card-body">
+              <form>
+                <div className="mb-3 text-center">
+                  <label htmlFor="username" className="form-label">
+                    Username
+                    <input type="text" className="form-control" id="username" />
+                  </label>
+                </div>
+                <div className="mb-3 text-center">
+                  <button type="submit" className="btn btn-primary">
+                    Sign In
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-link"
+                    onClick={handleForgotPassword}
+                  >
+                    Forgot Password?
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LoginPage;
