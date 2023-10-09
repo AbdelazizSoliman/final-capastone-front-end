92f039390f36525e66b734d9e08225c21606c34c
techmoves <muhndlovu28@gmail.com>
2023-10-09 13:08:31 +0200

    Add dependenses

---
+++ src/login.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const LoginPage = () => (
+  <div className="flex justify-center items-center h-screen">
+    <div className="w-1/3">
+      <div className="bg-white shadow-md rounded px-8 py-6">
+        <h3 className="text-2xl font-bold mb-6 text-center">Login</h3>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+);
+
+export default LoginPage;
