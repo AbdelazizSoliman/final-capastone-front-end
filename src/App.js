d524125a3d041fb41b233ed8ad9b9dc016b92d19
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-10-10 18:45:02 +0100

    Add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Main/Home';
+import DoctorsList from './components/doctors/DoctorsList';
+import DoctorsDetails from './components/doctors/DoctorDetails';
+import AddDoctor from './components/doctors/AddDoctor';
+import DeleteDoctor from './components/doctors/DeleteDoctor';
+import AppointmentsList from './components/appointments/AppointmentsList';
+import SpecializationsList from './components/specializations/SpecialisationsList';
+import LoginPage from './components/patients/LoginPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App" />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/doctors" element={<DoctorsList />} />
+      <Route path="/appointments" element={<AppointmentsList />} />
+      <Route path="/doctors/destails" element={<DoctorsDetails />} />
+      <Route path="/addnewdoctor" element={<AddDoctor />} />
+      <Route path="/deletedoctor" element={<DeleteDoctor />} />
+      <Route path="/specializations" element={<SpecializationsList />} />
+    </Routes>
   );
 }
 
