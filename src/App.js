de094ab81d69e15b6542f96aeea24644cc202d67
David Katende <katendedavidjoga@gmail.com>
2023-10-11 11:16:24 +0300

    Add Side nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/Main/Home';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/main/Home';
 import DoctorsList from './components/doctors/DoctorsList';
 import DoctorsDetails from './components/doctors/DoctorDetails';
 import AddDoctor from './components/doctors/AddDoctor';
 import DeleteDoctor from './components/doctors/DeleteDoctor';
 import AppointmentsList from './components/appointments/AppointmentsList';
+import SideNav from './components/SideNav';
 import SpecializationsList from './components/specializations/SpecialisationsList';
 import LoginPage from './components/patients/LoginPage';
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/doctors" element={<DoctorsList />} />
-      <Route path="/appointments" element={<AppointmentsList />} />
-      <Route path="/doctors/destails" element={<DoctorsDetails />} />
-      <Route path="/addnewdoctor" element={<AddDoctor />} />
-      <Route path="/deletedoctor" element={<DeleteDoctor />} />
-      <Route path="/specializations" element={<SpecializationsList />} />
-    </Routes>
+    <div className="container">
+      <div className="logo">Logo</div>
+      <div className="App">
+        <SideNav />
+        <div className="w-full p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/doctors" element={<DoctorsList />} />
+            <Route path="/appointments" element={<AppointmentsList />} />
+            <Route path="/doctor/details" element={<DoctorsDetails />} />
+            <Route path="/adddoctor" element={<AddDoctor />} />
+            <Route path="/deletedoctor" element={<DeleteDoctor />} />
+            <Route path="/specializations" element={<SpecializationsList />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 }
 
