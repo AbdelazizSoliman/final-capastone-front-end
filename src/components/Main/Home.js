544d280e9a9c63bfe10debfcc4a6f33ad35a9fa6
David Katende <katendedavidjoga@gmail.com>
2023-10-11 11:28:48 +0300

    Fix linters

--- src/components/Main/Home.js
+++ src/components/Main/Home.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const home = () => (
   <div>
-    <nav>
-      <Link to="/doctors">Doctors</Link>
-      <Link to="/appointments">Appointments</Link>
-      <Link to="/addnewdoctor">Add doctor</Link>
-      <Link to="/deletedoctor">Delete doctor</Link>
-      <Link to="/specializations">Specializations</Link>
-    </nav>
+    Home Component
   </div>
 );
 
