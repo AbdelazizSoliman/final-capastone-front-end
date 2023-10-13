cfb57f779c1edf842daed726e74de7bbb8753969
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-10-13 04:20:19 +0100

    Display list of items with  delete button

--- src/routes/doctors/DeleteDoctor.js
+++ src/routes/doctors/DeleteDoctor.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import fetchDoctors, { deleteDoctor } from '../../redux/doctors/doctorThunk';
 import SideNav from '../../components/home/SideNav';
+import DoctorItem from '../../components/home/doctorItem';
 
-const DeleteDoctor = () => (
-  <div>
-    <SideNav />
-    <div>
-      Delete Doctor
+const DeleteDoctor = () => {
+  const dispatch = useDispatch();
+  const doctorsArray = useSelector((state) => state.doctors.doctors);
+
+  useEffect(() => {
+    if (doctorsArray.length === 0) {
+      dispatch(fetchDoctors());
+    }
+  }, [dispatch, doctorsArray]);
+
+  const handleDelete = (doctorId) => {
+    dispatch(deleteDoctor(doctorId));
+  };
+
+  return (
+    <div className="h-screen bg-green">
+      <SideNav />
+      {doctorsArray.length > 0 ? (
+        <ul className="slideshowSlider">
+          {doctorsArray.map((doctor) => (
+            <li key={doctor.id}>
+              <DoctorItem name={doctor.name} />
+              <button type="button" onClick={() => handleDelete(doctor.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>No doctors available</div>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 export default DeleteDoctor;
