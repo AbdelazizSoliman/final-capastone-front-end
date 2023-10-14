02d93d6e502055249e9e9c610d333b8581f3613b
Alpha7 <henryomondi6612@gmail.com>
2023-10-14 10:21:44 +0300

    Add doctors details route

--- src/routes/doctors/DoctorDetails.js
+++ src/routes/doctors/DoctorDetails.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import SideNav from '../../components/home/SideNav';
 
-const DoctorDetails = () => (
-  <div>
-    <SideNav />
-    <div>
-      Doctor details
+function DoctorDetails() {
+  return (
+    <div className="h-screen flex lg:overflow-hidden bg-white">
+      {/* Static sidebar for desktop */}
+      <div className="hidden lg:flex lg:flex-shrink-0">
+        <SideBar />
+      </div>
+      <div className="flex flex-col min-w-0 flex-1 lg:overflow-hidden mt-32 lg:mt-1">
+        {/* Hamburger for mobile */}
+        <HamBurger />
+        <div className="flex flex-col items-center">
+          <Details />
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 export default DoctorDetails;
